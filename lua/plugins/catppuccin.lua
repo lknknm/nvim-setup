36f7f7aff8235dd38cce96cf13132b31a73a51a1
lknknm <github.snugness349@passinbox.com>
2025-10-08 19:08:37 +0200

    add: gitsigns and tweak catppuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,19 +3,26 @@ return {
     name = "catppuccin",
     priority = 1000,
     opts = {
-        flavour = "mocha", -- Auto-applies Catppuccin Mocha
-        transparent_background = true, -- THIS IS REQUIRED
-        show_end_of_buffer = false, -- Optional: Disable weird ~ marks
-        term_colors = true, -- Ensure terminal colors match theme
-        integrations = { ... }, -- Keep your existing integrations (if any)
+        transparent_background = true, 
+        show_end_of_buffer = false,
+        term_colors = true, 
+        integrations = {
+            gitsigns = true,
+            telescope = true,
+        },
     },
+
     config = function(_, opts)
         -- Apply transparency tweaks
         require("catppuccin").setup(opts)
         vim.cmd.colorscheme("catppuccin")
 
-        -- (Optional) Force other highlights to be transparent (if theme doesn't set them)
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        
+        -- transparent background
+        vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'TelescopeBorder', { link = "Function"  })
+        vim.api.nvim_set_hl(0, 'TelescopeTitle', { bg = 'none' })
     end
 }
