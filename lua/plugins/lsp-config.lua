40565cd11a6b8adfa92b37eef4be782137247133
lknknm <github.snugness349@passinbox.com>
2025-10-09 01:40:03 +0200

    add: lsp-config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls" },
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        vim.lsp.config('lua_ls', {}),
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {}),
+        vim.keymap.set('n', '<leader>bd', vim.lsp.buf.definition, {} ),
+        vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {} )
+    }
+}
