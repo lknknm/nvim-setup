da6f3ef41b232f6d1e12736f7be39508ce5038ef
lknknm <github.snugness349@passinbox.com>
2025-10-09 23:55:52 +0200

    add: vim-fugitive

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,12 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-        require("gitsigns").setup()
-    end
+    {
+        "tpope/vim-fugitive",
+        vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { desc = 'Git blame' })
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
+        end
+    }
 }
