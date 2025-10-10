32e1e5e8ee51bb21ae7b2f1d924c1c740d1920d8
lknknm <github.snugness349@passinbox.com>
2025-10-10 11:44:10 +0200

    update: blink and lsp-config configurations

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,8 +1,8 @@
 return {
   {
-    "Saghen/blink.cmp",
+    "saghen/blink.cmp",
     build = 'cargo build --release',
-    version = '*',
+    version = '1.*',
     dependencies = {
       -- Add blink-cmp-unreal as a dependency
       { "taku25/blink-cmp-unreal" },
@@ -10,17 +10,15 @@ return {
     opts = {
       sources = {
         -- Enable the "unreal" source
-        default = { "lsp", "buffer", "path", "unreal" },
+        default = { "lsp", "path", "buffer", "path", "unreal" },
         providers = {
           unreal = {
             module = "blink-cmp-unreal",
             name = "unreal",
-            -- Adjust the score if you want to prioritize it over LSP
             score_offset = 15,
           },
         },
       },
-      --... other blink.cmp settings
     },
   },
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -15,10 +15,31 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
-        vim.lsp.config('lua_ls', {}),
-        vim.lsp.config('clangd', {}),
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {}),
-        vim.keymap.set('n', '<leader>bd', vim.lsp.buf.definition, {} ),
-        vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {} )
+        dependencies = { 'saghen/blink.cmp' },
+        config = function()
+            -- Global keymaps
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', '<leader>bd', vim.lsp.buf.definition, {})
+            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+
+            -- LSP-specific configurations
+            vim.lsp.config('lua_ls', {})
+            vim.lsp.config('clangd', {
+                capabilities = require('blink.cmp').get_lsp_capabilities(),
+                cmd = {
+                    'clangd',
+                    '--compile-commands-dir=build',
+                    '--background-index',
+                    '--clang-tidy',
+                    '--header-insertion=iwyu',
+                    '--header-insertion-decorators',
+                },
+                on_attach = function(client, bufnr)
+                    -- Clangd-specific keymap
+                    vim.keymap.set('n', '<leader>sh', '<cmd>ClangdSwitchSourceHeader<cr>', { buffer = bufnr, desc = 'Switch between [S]ource and [H]eader' })
+                    vim.keymap.set('n', '<leader>fd', vim.lsp.buf.declaration, { desc = 'go to [f]unction [d]eclaration'})
+                end
+            })
+        end
     }
 }
