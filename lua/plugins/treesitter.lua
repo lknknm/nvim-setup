9f96dab44eb09056dc63fb721f32faa49f5ca10e
lknknm <github.snugness349@passinbox.com>
2025-11-16 01:48:42 +0100

    Add: none-ls

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,16 @@
-return { 
-    "nvim-treesitter/nvim-treesitter", 
-    branch = 'master', 
-    lazy = false, 
-    build = ":TSUpdate", 
-    config = function()
-       require("nvim-treesitter.configs").setup({
-           ensure_installed = { "asm", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "cpp" },
-           sync_install = false,
-           highlight = { enable = true },
-           indent = { enable = true }, 
-       }) 
-    end
+return {
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+            ensure_installed = { "asm", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "cpp" },
+            auto_install = true,
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
+
