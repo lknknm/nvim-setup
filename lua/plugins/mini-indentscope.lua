1e55aa8fcdc3f7451e6c2686a24ce5f5c08ee66a
lknknm <github.snugness349@passinbox.com>
2026-02-06 20:00:25 +0100

    Update: Dashboard art

--- lua/plugins/mini-indentscope.lua
+++ lua/plugins/mini-indentscope.lua
@@ -1,37 +1,55 @@
 return {
-    {
-        'nvim-mini/mini.indentscope', 
-        version = '*', 
-        config = function()
-            require('mini.indentscope').setup({
-                draw = {
-                    delay = 10,
-                    animation = function(s,n)
-                        return 0.5
-                    end,
-                },
-                opts = {
-                    border = 'both',
-                    indent_at_cursor = true,
-                    n_lines = 10000,
-                    try_as_border = true,
-                },
-                symbol = "│",
-            })
-        end
-    },
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        ---@module "ibl"
-        ---@type ibl.config
-        opts = {},
-        config = function ()
-            require("ibl").setup({
-                scope = { enabled = false },
-                indent = { char = "│" },
-            })
-        end
-    }
-}
+	{
+		"nvim-mini/mini.indentscope",
+		version = "*",
 
+		init = function()
+			-- Use the specific Snacks event for more reliable disabling
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "SnacksDashboardOpened",
+				callback = function()
+					vim.b.miniindentscope_disable = true
+				end,
+			})
+
+			-- Keep the FileType fallback for other non-text buffers
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+				callback = function()
+					vim.b.miniindentscope_disable = true
+				end,
+			})
+		end,
+
+		config = function()
+			require("mini.indentscope").setup({
+				draw = {
+					delay = 10,
+					animation = function(s, n)
+						return 0.5
+					end,
+				},
+				opts = {
+					border = "both",
+					indent_at_cursor = true,
+					n_lines = 10000,
+					try_as_border = true,
+				},
+				symbol = "│",
+			})
+		end,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {},
+		config = function()
+			require("ibl").setup({
+				scope = { enabled = false },
+				indent = { char = "│" },
+			})
+		end,
+	},
+}
