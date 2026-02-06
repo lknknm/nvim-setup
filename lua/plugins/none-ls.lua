ceecaca3baacdeb4af1c0ce02deec943cd3d647b
lknknm <github.snugness349@passinbox.com>
2026-02-06 19:35:21 +0100

    update: none-ls config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,16 +2,16 @@ return {
 	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
-
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.clang_format,
-                null_ls.builtins.formatting.prettier,
-                null_ls.builtins.formatting.asmfmt,
+				null_ls.builtins.formatting.clang_format,
+				null_ls.builtins.formatting.prettier.with({
+					filetypes = { "html", "css", "javascript", "json", "yaml", "markdown" },
+				}),
+				null_ls.builtins.formatting.asmfmt,
 			},
 		})
-
-		vim.keymap.set("n", "<leader>bf", vim.lsp.buf.format, {})
+		vim.keymap.set("n", "<leader>bf", vim.lsp.buf.format, { noremap = true, silent = true })
 	end,
 }
